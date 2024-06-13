84d31e17133838d49b6515aedd3dcb42ed72fc6b
BegzodAlimov <begzodalimov614@gmail.com>
2024-06-14 02:27:02 +0500

    created first app

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <Button type="submit" variant={"destructive"} className="m-20">submit</Button>
+    </main>
+  );
+}
